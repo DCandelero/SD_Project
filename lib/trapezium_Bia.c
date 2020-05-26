c2ba0333845dbbde8d11372e94d6b895bea8ca3f
david candelero <davidcandelero557@gmail.com>
2020-05-26 19:53:54 -0300

    Add files via upload

---
+++ lib/trapezium_Bia.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+double integration(double x){
+    //This function is responsible for calculating the function for a certain x value
+    double y = 0;
+    y = sqrt((100*100) - x*x);
+    return y;
+};
+
+double trapeziumCalc(double xi, double xf, double j){
+    /* j = discretization interval
+     * xi = initial x
+     * xf = final x
+     */
+    int i;
+    double h, n;
+    double x, xn, sum = 0, result1 = 0, result2 = 0;
+    
+    //n = number of subdivisions between xf and xi
+    n = (xf-xi)/j; 
+    // h = trapezium height
+    h = (xf - xi)/n;
+    //The calculum is initialized with the first value for x, or xi.
+    x = xi;
+    while(x <= (xf-j)){
+        result1 = integration(x);
+        x = x + j;
+        result2 = integration(x);
+        sum = sum + (h/2)*(result1 + result2);
+    }
+    
+    return sum;
+}
+
+
+
+    
+
+  
