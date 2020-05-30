94c529fd2c732a5ffe6253b0cc6fafe3aea77f71
David Candelero <davidcandelero557@gmail.com>
2020-05-30 17:04:21 -0300

    update main (again)

--- src/main.c
+++ src/main.c
@@ -3,40 +3,36 @@
 
 
 int main() {
-    int slaveNum = 0, portNum = 0, i = 0;
+    int slaveNum = 0, portNum = 50000, i = 0;
     char strS[120], strS1[120], strM[120], strM1[120];
     double j;
 
     printf("digite o número de escravos:");
     scanf("%d", &slaveNum);
 
-    printf("digite a porta para a conexão:");
-    scanf("%d", &portNum);
-
     printf("digite o valor de discretização:");
     scanf("%lf", &j);
 
     // Compile files
     system("gcc ./slave.c -lm -o slave && gcc ./master.c -o master");
-    printf("chegou aqui feiao");
+
     for (i = 0; i < slaveNum; i++) {
         // Execute slaves in different terminals
-        sprintf(strS, "gnome-terminal -- bash -c \"./slave %d\"; exec bash", portNum+i);
+        sprintf(strS, "gnome-terminal -- bash -c \"./slave %d\"", portNum);
         system(strS);
 
-        // Optional
-        // sprintf(strS1, "xterm -e bash -c \"./slave %d\"; exec bash", portNum+i);
+        // optional
+        // sprintf(strS1, "xterm -e bash -c \"./slave %d\"; exec bash", portNum);
         // system(strS1);
     }
-    // Execute master in current terminal 
+        
+    // Execute master in current terminal
     sprintf(strM, "./master localhost %d %d %lf", portNum, slaveNum, j);
     system(strM);
-    
-    // Optional
-    // sprintf(strM1,"./master localhost %d %d %lf", portNum, slaveNum, j);
+
+    // optional
+    // sprintf(strM1, "./master %d %d %lf", portNum, slaveNum, j);
     // system(strM1);
-
-
 
     
 
